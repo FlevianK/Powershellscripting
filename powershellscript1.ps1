79ca1e52cdb33d8e6172a3d7383d06a2956dec22
FlevianK <flevian.kanaiza@andela.com>
2018-01-22 20:36:44 +0300

    Correct file type

---
+++ powershellscript1.ps1
@@ -0,0 +1,4 @@
+﻿Write-Host "Hello Kanaiza"
+Get-WmiObject -Class Win32_OperatingSystem –ComputerName localhost |
+Select-Object -Property CSName,@{n=”Last Booted”;
+e={[Management.ManagementDateTimeConverter]::ToDateTime($_.LastBootUpTime)}}
